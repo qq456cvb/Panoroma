a80e97e2db073924e1d3b86df87a5c5e40faae63
qq456cvb <447626601@qq.com>
2019-06-30 23:35:33 +0800

    reestimate RANSAC homography with all inliers

--- Panoroma/RANSAC.cpp
+++ Panoroma/RANSAC.cpp
@@ -13,6 +13,7 @@ Mat2d<float> RANSACHomoSolver::solve(const vector<Point> &pts1, const vector<Poi
     Mat2d<float> best_homo(3, 3);
     Eigen::MatrixXf A(2 * min_corres, 9);
     int best_match = std::numeric_limits<int>::min();
+    vector<int> inlier_idxs, best_idxs;
     for (size_t i = 0; i < n_trial; ++i) {
         int inlier_cnt = 0;
         for (size_t j = 0; j < min_corres; ++j) {
@@ -49,14 +50,43 @@ Mat2d<float> RANSACHomoSolver::solve(const vector<Point> &pts1, const vector<Poi
             float err = sqrt((x - pts1[k].x) * (x - pts1[k].x) + (y - pts1[k].y) * (y - pts1[k].y));
             if (err < err_thresh) {
                 inlier_cnt++;
+                inlier_idxs.push_back(static_cast<int>(k));
             }
         }
         if (inlier_cnt > best_match) {
             best_match = inlier_cnt;
-            // TODO: re-estimate homography with all inliers
-            std::copy(h.data(), h.data() + 9, best_homo.raw_ptr());
+            best_idxs = inlier_idxs;
             std::cout << "found best with inlier cnt: " << inlier_cnt << std::endl;
         }
+        inlier_idxs.clear();
     }
+    // re-estimate homography with all inliers
+    Eigen::MatrixXf A_best(2 * best_idxs.size(), 9);
+    for (size_t j = 0; j < best_idxs.size(); ++j) {
+        
+        A_best(2 * j, 0) = 0;
+        A_best(2 * j, 1) = 0;
+        A_best(2 * j, 2) = 0;
+        A_best(2 * j, 3) = -pts2[best_idxs[j]].x;
+        A_best(2 * j, 4) = -pts2[best_idxs[j]].y;
+        A_best(2 * j, 5) = -1;
+        A_best(2 * j, 6) = pts1[best_idxs[j]].y * pts2[best_idxs[j]].x;
+        A_best(2 * j, 7) = pts1[best_idxs[j]].y * pts2[best_idxs[j]].y;
+        A_best(2 * j, 8) = pts1[best_idxs[j]].y;
+        
+        A_best(2 * j + 1, 0) = pts2[best_idxs[j]].x;
+        A_best(2 * j + 1, 1) = pts2[best_idxs[j]].y;
+        A_best(2 * j + 1, 2) = 1;
+        A_best(2 * j + 1, 3) = 0;
+        A_best(2 * j + 1, 4) = 0;
+        A_best(2 * j + 1, 5) = 0;
+        A_best(2 * j + 1, 6) = -pts1[best_idxs[j]].x * pts2[best_idxs[j]].x;
+        A_best(2 * j + 1, 7) = -pts1[best_idxs[j]].x * pts2[best_idxs[j]].y;
+        A_best(2 * j + 1, 8) = -pts1[best_idxs[j]].x;
+    }
+    Eigen::JacobiSVD<Eigen::MatrixXf> svd(A_best, Eigen::ComputeFullV);
+    Eigen::VectorXf tmp = svd.matrixV().rightCols(1);
+    Eigen::Map<Eigen::Matrix<float, 3, 3, Eigen::RowMajor>> h(tmp.data());
+    std::copy(h.data(), h.data() + 9, best_homo.raw_ptr());
     return best_homo;
 }
